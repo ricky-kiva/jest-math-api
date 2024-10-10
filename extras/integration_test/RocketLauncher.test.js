a37d1b95f61b7d1ed4a45a283a583296aec2f135
ricky-kiva <ricky.kiva@gmail.com>
2024-10-10 22:37:24 +0700

    test(extras): add test for `launchByQueue`

--- extras/integration_test/RocketLauncher.test.js
+++ extras/integration_test/RocketLauncher.test.js
@@ -16,4 +16,17 @@ describe('A RocketLauncher', () => {
     expect(spaceXRocket.engineStatus).toEqual('active');
     expect(rocketLauncher.rockets.length).toEqual(0);
   });
+
+  it('can launch one rocket by queue', () => {
+    const nasaRocket = new Rocket('NASA');
+    const spaceXRocket = new Rocket('SpaceX');
+
+    const rocketLauncher = new RocketLauncher([nasaRocket, spaceXRocket]);
+
+    rocketLauncher.launchByQueue();
+
+    expect(nasaRocket.engineStatus).toEqual('active');
+    expect(spaceXRocket.engineStatus).toEqual('inactive');
+    expect(rocketLauncher.rockets.length).toEqual(1);
+  });
 });
