8707b12949c225bf46c8dd68ea41a123990e35e6
ricky-kiva <ricky.kiva@gmail.com>
2024-10-16 11:04:21 +0700

    fix(src): incorrect func. name

--- src/MathBasic.test.js
+++ src/MathBasic.test.js
@@ -5,12 +5,12 @@ const MathBasic = require('./MathBasic');
 describe('A MathBasic object', () => {
   it('should contain add, substract, multiply, & divide function', () => {
     expect(MathBasic).toHaveProperty('add');
-    expect(MathBasic).toHaveProperty('substract');
+    expect(MathBasic).toHaveProperty('subtract');
     expect(MathBasic).toHaveProperty('multiply');
     expect(MathBasic).toHaveProperty('divide');
 
     expect(MathBasic.add).toBeInstanceOf(Function);
-    expect(MathBasic.substract).toBeInstanceOf(Function);
+    expect(MathBasic.subtract).toBeInstanceOf(Function);
     expect(MathBasic.multiply).toBeInstanceOf(Function);
     expect(MathBasic.divide).toBeInstanceOf(Function);
   });
