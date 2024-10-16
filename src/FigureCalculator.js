e03473179ff0355e55c0ecc3cdba6456ce8fa7c5
ricky-kiva <ricky.kiva@gmail.com>
2024-10-16 11:14:37 +0700

    feat(src): complete `FigureCalculator` class

--- src/FigureCalculator.js
+++ src/FigureCalculator.js
@@ -5,30 +5,43 @@ class FigureCalculator {
     this._mathBasic = mathBasic;
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  _verifyArgs(args, expectedArgsCount) {
+    if (args.length !== expectedArgsCount) {
+      throw new Error(`function only accept ${expectedArgsCount} arguments`);
+    }
+
+    args.forEach((arg) => {
+      if (typeof arg !== 'number') {
+        throw new Error('function only accept number type arguments');
+      }
+    });
+
+    return args;
+  }
+
   calculateRectanglePerimeter(...args) {
-    if (args.length !== 2) {
-      throw new Error('function could only receive 2 parameters');
-    }
-
-    const [length, width] = args;
-
-    if (typeof length !== 'number' || typeof width !== 'number') {
-      throw new Error('function could only accept number arguments');
-    }
+    const [length, width] = this._verifyArgs(args, 2);
 
     return this._mathBasic.multiply(2, this._mathBasic.add(length, width));
   }
 
-  calculateRectangleArea() {
+  calculateRectangleArea(...args) {
+    const [length, width] = this._verifyArgs(args, 2);
 
+    return this._mathBasic.multiply(length, width);
   }
 
-  calculateTrianglePerimeter() {
+  calculateTrianglePerimeter(...args) {
+    const [sideA, sideB, base] = this._verifyArgs(args, 3);
 
+    return this._mathBasic.add(sideA, this._mathBasic.add(sideB, base));
   }
 
-  calculateTriangleArea() {
+  calculateTriangleArea(...args) {
+    const [base, height] = this._verifyArgs(args, 2);
 
+    return this._mathBasic.divide(this._mathBasic.multiply(base, height), 2);
   }
 }
 
