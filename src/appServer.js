fcbfd91fce31cb302adfb9e112f4204f5e7acd1d
ricky-kiva <ricky.kiva@gmail.com>
2024-10-29 19:08:37 +0700

    feat(server): impl. complete math API set

--- src/appServer.js
+++ src/appServer.js
@@ -2,7 +2,7 @@
 
 const Hapi = require('@hapi/hapi');
 
-const appServer = ({ mathBasic }) => {
+const appServer = ({ mathBasic, figureCalculator }) => {
   const server = Hapi.server({
     host: 'localhost',
     port: 5000
@@ -24,6 +24,64 @@ const appServer = ({ mathBasic }) => {
       handler: (req) => {
         const { a, b } = req.params;
         const value = mathBasic.subtract(Number(a), Number(b));
+
+        return { value };
+      }
+    }, {
+      method: 'GET',
+      path: '/multiply/{a}/{b}',
+      handler: (request) => {
+        const { a, b } = request.params;
+        const value = mathBasic.multiply(Number(a), Number(b));
+
+        return { value };
+      }
+    }, {
+      method: 'GET',
+      path: '/divide/{a}/{b}',
+      handler: (request) => {
+        const { a, b } = request.params;
+        const value = mathBasic.divide(Number(a), Number(b));
+
+        return { value };
+      }
+    }, {
+      method: 'GET',
+      path: '/rectangle/perimeter/{length}/{width}',
+      handler: (req) => {
+        const { length, width } = req.params;
+        const value = figureCalculator
+          .calculateRectanglePerimeter(Number(length), Number(width));
+
+        return { value };
+      }
+    }, {
+      method: 'GET',
+      path: '/rectangle/area/{length}/{width}',
+      handler: (req) => {
+        const { length, width } = req.params;
+        const value = figureCalculator
+          .calculateRectangleArea(Number(length), Number(width));
+
+        return { value };
+      }
+    }, {
+      method: 'GET',
+      path: '/triangle/perimeter/{sideA}/{sideB}/{base}',
+      handler: (req) => {
+        const { sideA, sideB, base } = req.params;
+        const value = figureCalculator
+          .calculateTrianglePerimeter(Number(sideA), Number(sideB), Number(base));
+
+        return { value };
+      }
+    }, {
+      method: 'GET',
+      path: '/triangle/area/{base}/{height}',
+      handler: (req) => {
+        const { base, height } = req.params;
+        const value = figureCalculator
+          .calculateTriangleArea(Number(base), Number(height));
 
         return { value };
       }
